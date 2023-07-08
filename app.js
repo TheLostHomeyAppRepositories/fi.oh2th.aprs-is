1b9388c61c63e1bbb1055b22030fd57654c0ea06
Tapio Heiskanen <tapio.heiskanen@iki.fi>
2023-07-08 08:04:37 +0300

    Add sentry.io crash logging for uncaught rejectoions.

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 'use strict';
 
 const Homey = require('homey');
+const { Log } = require('homey-log');
 const flowActions = require('./lib/flows/actions');
 
 module.exports = class AprsApp extends Homey.App {
@@ -10,6 +11,8 @@ module.exports = class AprsApp extends Homey.App {
    */
   async onInit() {
     this.myAppIdVersion = `${this.homey.manifest.id}/${this.homey.manifest.version}`;
+    this.homeyLog = new Log({ homey: this.homey });
+
     this.log(`${this.myAppIdVersion} - onInit - starting...`);
 
     await flowActions.init(this);
